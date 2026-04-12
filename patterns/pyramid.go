056ee58697174bceed5a9f4eab16ebe8e1868ede
Neeli Mahi Teja <mahineeli899@gmail.com>
2026-04-12 23:26:45 +0530

    new: add pyramid pattern printing

---
+++ patterns/pyramid.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func pyramid(){
+	n:= 10
+	for i := 1; i <=n; i++ {
+		for space := 0; space <= n-i-1; space++ {
+		   fmt.Print(`   `)
+		}
+		for j := 1; j <=2*i-1; j++ {
+			fmt.Print(` * `)
+			
+		}
+		for space := 0; space <= n-i-1; space++ {
+		   fmt.Print(`  `)
+		}
+			 
+		fmt.Println()
+	}
+}

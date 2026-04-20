dc099236ff75676d1646110aec078a79f1af5d66
Neeli Mahi Teja <mahineeli899@gmail.com>
2026-04-20 22:30:30 +0530

    new: add BTNumbers and BTNumbersRev functions for recursive number printing

---
+++ reccursion/backTrackingNumbers.go
@@ -0,0 +1,24 @@
+package reccursion
+
+import "fmt"
+
+
+func BTNumbers(i int,n int){
+
+	if(n<1){
+		return
+	}
+	BTNumbers(i,n-1)
+	fmt.Println(n)
+}
+func BTNumbersRev(i int,n int){
+
+	if(i>n){
+		return
+	}
+
+	BTNumbersRev(i+1,n)
+	fmt.Println(i)
+}
+// 1,5. 
+// 

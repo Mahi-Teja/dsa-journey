1268996f9c987cdfcf6e1503624f5f6efdc7b9c9
Neeli Mahi Teja <mahineeli899@gmail.com>
2026-04-30 19:36:29 +0530

    refactor TwoSum function to improve comments on time and space complexity

--- arrays/twoSum.go
+++ arrays/twoSum.go
@@ -1,7 +1,9 @@
 package arrays
  
 func TwoSum(nums []int, target int) []int { 
-    // indecise sum = target
+    // indecise sum = target : brute force
+    // tc O(n^2)
+    //  sc O(n)
     for  i :=0;i<len(nums);i++{ 
         for j:=0;j<len(nums);j++{
 			  if(i==j) {continue}
